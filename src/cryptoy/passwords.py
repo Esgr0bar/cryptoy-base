c2218967c62e2a859c5a000975c9b62b0d4b06fd
Esgr0bar <163046224+Esgr0bar@users.noreply.github.com>
2024-08-01 11:28:04 +0200

    Update passwords.py

--- src/cryptoy/passwords.py
+++ src/cryptoy/passwords.py
@@ -6,14 +6,11 @@ from random import (
 
 import names
 
-
 def hash_password(password: str) -> str:
     return hashlib.sha3_256(password.encode()).hexdigest()
 
-
 def random_salt() -> str:
     return bytes.hex(os.urandom(32))
-
 
 def generate_users_and_password_hashes(
     passwords: list[str], count: int = 32
@@ -26,37 +23,41 @@ def generate_users_and_password_hashes(
     }
     return users_and_password_hashes
 
-
 def attack(passwords: list[str], passwords_database: dict[str, str]) -> dict[str, str]:
     users_and_passwords = {}
 
-    # A implémenter
-    # Doit calculer le mots de passe de chaque utilisateur grace à une attaque par dictionnaire
+    for user, password_hash in passwords_database.items():
+        for password in passwords:
+            if hash_password(password) == password_hash:
+                users_and_passwords[user] = password
+                break
 
     return users_and_passwords
-
 
 def fix(
     passwords: list[str], passwords_database: dict[str, str]
 ) -> dict[str, dict[str, str]]:
     users_and_passwords = attack(passwords, passwords_database)
 
-    users_and_salt = {}
     new_database = {}
 
-    # A implémenter
-    # Doit calculer une nouvelle base de donnée ou chaque élement est un dictionnaire de la forme:
-    # {
-    #     "password_hash": H,
-    #     "password_salt": S,
-    # }
-    # tel que H = hash_password(S + password)
+    for user, password in users_and_passwords.items():
+        salt = random_salt()
+        salted_password = salt + password
+        password_hash = hash_password(salted_password)
+        new_database[user] = {
+            "password_hash": password_hash,
+            "password_salt": salt,
+        }
 
     return new_database
-
 
 def authenticate(
     user: str, password: str, new_database: dict[str, dict[str, str]]
 ) -> bool:
-    # Doit renvoyer True si l'utilisateur a envoyé le bon password, False sinon
-    pass
+    if user not in new_database:
+        return False
+
+    user_data = new_database[user]
+    salted_password = user_data["password_salt"] + password
+    return hash_password(salted_password) == user_data["password_hash"]
