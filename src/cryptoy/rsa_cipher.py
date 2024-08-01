cf2073c76938798b5b6dc00f51ccad7743c2bb7a
Esgr0bar <163046224+Esgr0bar@users.noreply.github.com>
2024-08-01 11:26:30 +0200

    Update rsa_cipher.py

--- src/cryptoy/rsa_cipher.py
+++ src/cryptoy/rsa_cipher.py
@@ -1,7 +1,4 @@
-from math import (
-    gcd,
-)
-
+from math import gcd
 from cryptoy.utils import (
     draw_random_prime,
     int_to_str,
@@ -10,27 +7,38 @@ from cryptoy.utils import (
     str_to_int,
 )
 
-
 def keygen() -> dict:
     e = 65537
-    # Implementez la génération de clef de RSA avec e = 65537
-    # 1. Tire aléatoirement un nombre premier p avec la fonction draw_random_prime
-    # 2. Tire aléatoirement un nombre premier q avec la fonction draw_random_prime
-    # 3. Calcul de d, l'inverse de e modulo (p - 1) * (q - 1), avec la fonction modular_inverse
-    # 4. Renvoit un dictionnaire { "public_key": (e, p * q), "private_key": d}
-    pass
 
+    p = draw_random_prime()
+    q = draw_random_prime()
+
+    n = p * q
+    phi_n = (p - 1) * (q - 1)
+
+    d = modular_inverse(e, phi_n)
+
+    return {
+        "public_key": (e, n),
+        "private_key": d
+    }
 
 def encrypt(msg: str, public_key: tuple) -> int:
-    # Implementez le chiffrement rsa d'un message avec une clef publique de la forme (e, N)
-    # 1. Convertir le message en nombre entier avec la fonction str_to_int
-    # 2. Verifiez que ce nombre est < public_key[1], sinon lancer une exception
-    # 3. Chiffrez le nombre entier avec pow_mod et les paramètre de la clef publique (e, N)
-    pass
+    e, n = public_key
 
+    m = str_to_int(msg)
+
+    if m >= n:
+        raise ValueError("Le message est trop long pour être chiffré avec la clé publique donnée.")
+
+    c = pow_mod(m, e, n)
+    return c
 
 def decrypt(msg: int, key: dict) -> str:
-    # Implementez le dechiffrement rsa d'un message avec une clef de la forme { "public_key": (e, p * q), "private_key": d}
-    # 1. Utilisez pow_mod avec les paramètres de la clef
-    # 2. Convertir l'entier calculé en str avec la fonction int_to_str
-    pass
+    public_key = key["public_key"]
+    d = key["private_key"]
+    n = public_key[1]
+
+    m = pow_mod(msg, d, n)
+
+    return int_to_str(m
